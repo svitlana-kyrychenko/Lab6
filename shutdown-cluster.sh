32542019e3a5d7e79f31459ed7ac65f5d2bfcb6b
svitlana-kyrychenko <102665740+svitlana-kyrychenko@users.noreply.github.com>
2022-06-10 20:13:10 +0300

    Update shutdown-cluster.sh

--- shutdown-cluster.sh
+++ shutdown-cluster.sh
@@ -1,8 +1,1 @@
 docker-compose down
-docker stop kafka-server
-docker stop zookeeper-server
-docker rm kafka-server
-docker rm zookeeper-server
-docker image rm bitnami/kafka
-docker image rm bitnami/zookeeper
-docker network rm kafka-network
